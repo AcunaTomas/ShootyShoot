4c02a2933dda43c29e145065784a3f6109e424c3
AcunaTomas <xxtomyacuxx@gmail.com>
2022-09-28 13:59:04 -0300

    Test PowerUP

--- Assets/Scripts/Misc/PPup.cs
+++ Assets/Scripts/Misc/PPup.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class PPup : MonoBehaviour
 {
-
-    void Start()
+    public int mode = 1;
+    
+    void OnTriggerEnter(Collider other)
     {
-        
+        if (other.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
     }
-
     void Update()
     {
         transform.Translate(0,-2 * Time.deltaTime,0);
